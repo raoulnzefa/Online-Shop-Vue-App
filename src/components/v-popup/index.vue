ec1014c37f4ba4db9ee6a6d509566b4a19caf287
Olga Volosevich <olga.lucky.v@gmail.com>
2020-06-24 18:13:38 +0300

    feat: add closing item-info modal on background click

--- src/components/v-popup/index.vue
+++ src/components/v-popup/index.vue
@@ -1,29 +1,35 @@
 <template>
-    <div class="v-popup">
-        <div class="v-popup__header">
-            <span>Product details</span>
-            <i 
-            class="material-icons"
-            @click="closeModal">close</i>
+   <div 
+   class="popup-wrapper" 
+   ref="popup_wrapper"
+   >
+        <div 
+        class="v-popup">
+            <div class="v-popup__header">
+                <span>Product details</span>
+                <i 
+                class="material-icons"
+                @click="closeModal">close</i>
+            </div>
+            <div class="v-popup__content">
+                <slot></slot>
+            </div>
+            <div class="v-popup__footer">
+                <button 
+                class="close-modal"
+                @click="closeModal"
+                >
+                Close
+                </button>
+                <button 
+                class="submit_btn"
+                @click="addToCart"
+                >
+                Add to cart
+                </button>
+            </div>
         </div>
-        <div class="v-popup__content">
-            <slot></slot>
-        </div>
-        <div class="v-popup__footer">
-            <button 
-            class="close-modal"
-            @click="closeModal"
-            >
-            Close
-            </button>
-            <button 
-            class="submit_btn"
-            @click="addToCart"
-            >
-            Add to cart
-            </button>
-        </div>
-    </div>
+   </div>
 </template>
 
 <style lang="scss">
@@ -50,7 +56,11 @@ export default {
         }
     },
     mounted() {
-        
+        document.addEventListener("click", (event)=> {
+            if (event.target === this.$refs["popup_wrapper"]) {
+                this.closeModal()
+            }
+        })
     },
 };
 </script>
