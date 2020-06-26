c5b4be6ea4020548ec9f0d077897219af09d1b81
Olga Volosevich <olga.lucky.v@gmail.com>
2020-06-26 18:50:35 +0300

    Initial dist subtree commit

---
+++ precache-manifest.4806567a54bd27f989ccf0a2131a3a80.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09600e4bad70dc2bdd15",
+    "url": "/Online-Shop-Vue-App/css/app.5db54d0f.css"
+  },
+  {
+    "revision": "17b8b707bac61d723111",
+    "url": "/Online-Shop-Vue-App/css/chunk-vendors.40df3b9d.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/Online-Shop-Vue-App/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/Online-Shop-Vue-App/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/Online-Shop-Vue-App/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/Online-Shop-Vue-App/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "8cadf2cc79f9a252cddf08b4638d2eb9",
+    "url": "/Online-Shop-Vue-App/img/1.8cadf2cc.jpg"
+  },
+  {
+    "revision": "cbe607fc043a2dde671ac986473e70f8",
+    "url": "/Online-Shop-Vue-App/img/2.cbe607fc.jpg"
+  },
+  {
+    "revision": "a41016bee4e785fa2758c903de49917e",
+    "url": "/Online-Shop-Vue-App/img/3.a41016be.jpg"
+  },
+  {
+    "revision": "eae4304ae23c8199201082798aec89be",
+    "url": "/Online-Shop-Vue-App/img/4.eae4304a.jpg"
+  },
+  {
+    "revision": "8fe7c7206ef5ca0f9f8457b0077daa94",
+    "url": "/Online-Shop-Vue-App/img/5.8fe7c720.jpg"
+  },
+  {
+    "revision": "3c3a467ef29c07a3d74d7abae9a06dd9",
+    "url": "/Online-Shop-Vue-App/img/6.3c3a467e.jpg"
+  },
+  {
+    "revision": "fb1dafa2d3d9789cc71227912ef1eeb1",
+    "url": "/Online-Shop-Vue-App/index.html"
+  },
+  {
+    "revision": "09600e4bad70dc2bdd15",
+    "url": "/Online-Shop-Vue-App/js/app.21ac7139.js"
+  },
+  {
+    "revision": "17b8b707bac61d723111",
+    "url": "/Online-Shop-Vue-App/js/chunk-vendors.b93db6da.js"
+  },
+  {
+    "revision": "9c2b1b1877010882ee1dcf62415c37b6",
+    "url": "/Online-Shop-Vue-App/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Online-Shop-Vue-App/robots.txt"
+  }
+]);
